48df335d69768abf3f9a215299198e02dd11aa7e
FerPBracamontes <>
2022-08-25 17:41:40 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+/*Hola Mundo
+//Fernanda Perez
+//25-08-22
+Versión 1*/
+
+//Bibliotecas
+#include <stdio.h>
+
+//Funcion principal
+int main(void) {
+
+  //funcion de impresion de pantalla
+  printf("Hola Mundo\n");
+  return 0;
+}
